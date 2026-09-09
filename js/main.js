6dec5c41510eddf22e40758653abbedda37156d6
teaizteaiz46-blip <teaizteaiz46@gmail.com>
2026-09-09 22:22:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+if(sb){
+  sb.auth.onAuthStateChange(async (event, session)=>{
+    if(session && session.user){
+      state.user = await loadOrCreateProfile(session.user);
+    } else {
+      state.user = null;
+    }
+    render();
+  });
+}
+
 render();
