c894e50cc54aa8d74f10fd73d7a2048c2f0579cb
ameerazax-hu <ameerazax@gmail.com>
2026-09-15 19:28:02 +0300

    صفحة الدعم

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ const path = require('path');
 const ROOT = __dirname;
 const OUT = path.join(ROOT, 'www');
 
-const FILES = ['index.html', 'styles.css', 'privacy.html', 'terms.html'];
+const FILES = ['index.html', 'styles.css', 'privacy.html', 'terms.html', 'support.html'];
 const DIRS = ['js', 'img'];
 
 function fail(msg) {
